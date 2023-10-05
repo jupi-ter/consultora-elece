bc2846c95cd38644b322d9101559963d3d7977b8
Yoprolezpy05 <94013174+Fabrilez05@users.noreply.github.com>
2023-10-05 11:31:14 -0300

    Add files via upload

--- podyecto/src/controlador/LoginController.java
+++ podyecto/src/controlador/LoginController.java
@@ -1,32 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package controlador;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class LoginController {
+/**
+ * FXML Controller class
+ *
+ * @author Administrator
+ */
+public class LoginController implements Initializable {
 
     @FXML
     private TextField usernameField;
-
     @FXML
     private PasswordField passwordField;
-
     @FXML
-    private void handleLogin() {
+    private Label statusLabel;
+    @FXML
+    private ImageView myImageView;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+       
+    }    
+
+        @FXML
+    private void loginButtonAction() {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        // You can add login logic here
-        // For simplicity, let's just print the input
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
+        //auth logic (rudimentary)
+        if (username.equals("admin") && password.equals("1234")) {
+            
+        } else {
+            statusLabel.setText("Usuario/Contraseña incorrectos");
+        }
     }
-
-    @FXML
-    private void handleCancel() {
-        // You can add cancel logic here
-        System.out.println("Login cancelled.");
+    
+    private void displayImage(){
+        Image myImage = new Image(getClass().getResourceAsStream("vistas/ELECE_logo.jpeg"));
+        myImageView.setImage(myImage);
     }
+    
 }
