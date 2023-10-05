a4a8290f417b787703457c0484484b502f63c189
jules <102880647+jupi-ter@users.noreply.github.com>
2023-10-05 10:44:29 -0300

    Add files via upload

--- podyecto/src/consultora/JavaFXApplication1.java
+++ podyecto/src/consultora/JavaFXApplication1.java
@@ -14,32 +14,26 @@ import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import modelo.conexion;
 
-/**
- *
- * @author TERCEROS TM
- */
 public class JavaFXApplication1 extends Application {
     conexion con = new conexion();
     @Override
     public void start(Stage stage) throws Exception {
         if (con.getCon() == null) {
             Alert alerta = new Alert(Alert.AlertType.ERROR);
-            alerta.setTitle("El sistema comunica");
-            alerta.setContentText("Ndi");
+            alerta.setTitle("No conectado");
+            alerta.setContentText("Contacte a un supervisor.");
             alerta.setHeaderText(null);
             alerta.show();
         } else {
-            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
-            alerta.setTitle("El sistema comunica");
-            alerta.setContentText("Conectado.");
-            alerta.setHeaderText(null);
-            alerta.show();
+            Parent root = FXMLLoader.load(getClass().getResource("/vistas/login.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Login");
+            stage.setMaximized(false);
+            stage.show();
         }
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
---
+++ podyecto/src/controlador/LoginController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Administrator
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label statusLabel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+        @FXML
+    private void loginButtonAction() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        //auth logic (rudimentary)
+        if (username.equals("admin") && password.equals("1234")) {
+            
+        } else {
+            statusLabel.setText("Usuario/Contraseña incorrectos");
+        }
+    }
+    
+}
