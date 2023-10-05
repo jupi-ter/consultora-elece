ced86e2ec369e5368982e61bd71189fe5a1e86a8
Yoprolezpy05 <94013174+Fabrilez05@users.noreply.github.com>
2023-10-05 13:29:38 -0300

    Add files via upload

---
+++ podyecto/src/controlador/PedidoController.java
@@ -0,0 +1,82 @@
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Administrator
+ */
+public class PedidoController implements Initializable{ 
+
+    @FXML
+    private Button scene1Button;
+
+    @FXML
+    private Button scene2Button;
+
+    @FXML
+    private Button scene3Button;
+
+    @FXML
+    private Button pedidosButton;
+
+    @FXML
+    private ComboBox<String> comboBox1;
+
+    @FXML
+    private ComboBox<String> comboBox2;
+
+    @FXML
+    private DatePicker calendar1;
+
+    @FXML
+    private DatePicker calendar2;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private TextField descriptionTextField;
+
+    @FXML
+    private Button nuevoButton;
+
+    @FXML
+    private Button modificarButton;
+
+    @FXML
+    private Button borrarButton;
+
+    @FXML
+    private Button cancelarButton;
+    
+    @FXML
+    private ImageView myImageView;
+
+   
+    /**
+     * Initializes the controller class.
+     * @param url
+     */
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    private void displayImage(){
+        Image myImage = new Image(getClass().getResourceAsStream("vistas/ELECE_logo.jpeg"));
+        myImageView.setImage(myImage);
+    }
+    
+}    
+   
